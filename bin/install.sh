6dfcb6f0b5cb392356dae01388e26f973228c6e9
Cyd Noxzed <cyd@noxzed.de>
2015-05-20 12:01:52 +0200

    add overviewer subdirectories to installer

--- bin/install.sh
+++ bin/install.sh
@@ -22,7 +22,8 @@ do_mkdirs() {
 	mkdir -p -v  "$_ROOT/backups/$_INSTANCE/"
 	mkdir -p -v  "$_ROOT/logs/"
 	mkdir -p -v  "$_ROOT/tmp/"
-	mkdir -p -v  "$_ROOT/overviewer/$_INSTANCE/"
+	mkdir -p -v  "$_ROOT/overviewer/$_INSTANCE/mapcopy"
+	mkdir -p -v  "$_ROOT/overviewer/$_INSTANCE/output"
 
 cat <<EOF > "$_ROOT/bin/minecraftd.$_INSTANCE.sh"
 #!/bin/sh
